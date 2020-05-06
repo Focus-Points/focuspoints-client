8a4681883ad01c5a34ad0b5736c1f8199a2496d2
René Kamp <r.kamp@tfe.nl>
2020-05-06 11:52:06 +0200

    Fixed configuration. Expose configuration to servlet context.

--- focuspoints-client-core-spring-boot-starter/src/main/java/io/focuspoints/client/FocusPointsAutoConfiguration.java
+++ focuspoints-client-core-spring-boot-starter/src/main/java/io/focuspoints/client/FocusPointsAutoConfiguration.java
@@ -1,21 +1,16 @@
 package io.focuspoints.client;
 
-import io.focuspoints.client.TokenCreator;
-import io.focuspoints.client.UrlCreator;
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.context.support.ServletContextAttributeExporter;
 
 @Configuration
-@EnableConfigurationProperties
+@EnableConfigurationProperties(FocusPointsAutoConfigurationProperties.class)
 public class FocusPointsAutoConfiguration {
-
-	@Bean
-	@ConditionalOnMissingBean
-	public FocusPointsConfigurationProperties focusPointsConfigurationProperties() {
-		return new FocusPointsConfigurationProperties();
-	}
 
 	@Bean(destroyMethod = "destroy")
 	@ConditionalOnMissingBean(name = "io.focuspoints.client.TokenCreator")
@@ -27,5 +22,18 @@ public class FocusPointsAutoConfiguration {
 	@ConditionalOnMissingBean(name = "io.focuspoints.client.URLCreator")
 	public UrlCreator urlCreator(FocusPointsConfigurationProperties focusPointsConfigurationProperties) {
 		return new UrlCreator(focusPointsConfigurationProperties);
+	}
+	
+	@Bean
+	public ServletContextAttributeExporter servletContextAttributeExporter(
+			FocusPointsAutoConfigurationProperties focusPointsConfigurationProperties) {
+		
+		Map<String, Object> attributes = new HashMap<>();
+		attributes.put(FocusPointsConfigurationProperties.class.getName(), focusPointsConfigurationProperties);
+		
+		ServletContextAttributeExporter exporter = new ServletContextAttributeExporter();
+		exporter.setAttributes(attributes);
+		
+		return exporter;
 	}
 }
