478a8c0760a0c01c839e7be4b7efb64535d3c034
René Kamp <r.kamp@tfe.nl>
2020-05-06 11:53:08 +0200

    Expose configuration to servlet context

--- focuspoints-client-core-servlet/src/main/java/io/focuspoints/servlet/FocusPointsInitalizer.java
+++ focuspoints-client-core-servlet/src/main/java/io/focuspoints/servlet/FocusPointsInitalizer.java
@@ -4,6 +4,7 @@ import io.focuspoints.client.FocusPointsConfigurationProperties;
 import io.focuspoints.client.TokenCreator;
 import io.focuspoints.client.UrlCreator;
 import java.util.function.Consumer;
+import java.util.function.Function;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -15,23 +16,27 @@ public class FocusPointsInitalizer implements ServletContextListener {
 
 	@Override
 	public void contextInitialized(ServletContextEvent sce) {
+		ServletContext sc = sce.getServletContext();
+		
 		FocusPointsConfigurationProperties focusPointsConfigurationProperties =
-				getFocusPointsConfigurationProperties(sce);
+				getFocusPointsConfigurationProperties(sc);
+		
+		sc.setAttribute(FocusPointsConfigurationProperties.class.getName(), focusPointsConfigurationProperties);
 		
 		new TokenCreator(focusPointsConfigurationProperties);
 		new UrlCreator(focusPointsConfigurationProperties);
 	}
+	
 	@Override
 	public void contextDestroyed(ServletContextEvent sce) {
 		TokenCreator.destroy();
 		UrlCreator.destroy();
 	}
 	
-	private static FocusPointsConfigurationProperties getFocusPointsConfigurationProperties(ServletContextEvent sce) {
-		ServletContext sc = sce.getServletContext();
-		
+	private static FocusPointsConfigurationProperties getFocusPointsConfigurationProperties(ServletContext sc) {
 		FocusPointsConfigurationProperties properties = new FocusPointsConfigurationProperties();
 		
+		setPropertyIfExists(sc, "focuspoints.enabled", properties::setEnabled, Boolean::valueOf);
 		setPropertyIfExists(sc, "focuspoints.url", properties::setUrl);
 		setPropertyIfExists(sc, "focuspoints.token-request-parameter-name", properties::setTokenRequestParameterName);
 		setPropertyIfExists(sc, "focuspoints.token-id", properties::setTokenId);
@@ -41,10 +46,22 @@ public class FocusPointsInitalizer implements ServletContextListener {
 	}
 	
 	private static void setPropertyIfExists(ServletContext sc, String propertyName, Consumer<String> consumer) {
+		setPropertyIfExists(sc, propertyName, consumer, String::valueOf);
+	}
+	
+	private static <T> void setPropertyIfExists(ServletContext sc, String propertyName, Consumer<T> consumer, Function<String, T> converter) {
 		String propertyValue = sc.getInitParameter(propertyName);
 		
-		if (StringUtils.isNotBlank(propertyValue)) {
-			consumer.accept(propertyValue);
+		if(StringUtils.isBlank(propertyValue)) {
+			return;
 		}
+		
+		T propertyValueConverted = converter.apply(propertyValue);
+		
+		if(propertyValueConverted == null) {
+			return;
+		}
+		
+		consumer.accept(propertyValueConverted);
 	}
 }
